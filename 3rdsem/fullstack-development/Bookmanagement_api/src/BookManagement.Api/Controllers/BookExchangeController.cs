06332efdb9d40d9712c1f5ef3b3cf803c6e10164
Raghunath <raghunathec@gmail.com>
2024-11-06 08:37:14 +0530

    clean code

--- 3rdsem/fullstack-development/Bookmanagement_api/src/BookManagement.Api/Controllers/BookExchangeController.cs
+++ 3rdsem/fullstack-development/Bookmanagement_api/src/BookManagement.Api/Controllers/BookExchangeController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookManagement.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class BookExchangeController : ControllerBase
     {
