69a64f7159bf57d65ac373053904d15b0c91337e
Raghunath <raghunathec@gmail.com>
2024-11-16 09:56:53 +0530

    refactor

--- 3rdsem/fullstack-development/frontend/book-exchange-liberary/client/src/App.js
+++ 3rdsem/fullstack-development/frontend/book-exchange-liberary/client/src/App.js
@@ -25,14 +25,6 @@ function App() {
       <Router>
         <div className="App">
           <header className="bg-primary text-white text-center py-3"><h1>Book Exchange Library </h1>
-            {/* <nav>
-             <ul className="nav justify-content-center">
-              <li className="nav-item"><Link className="nav-link text-white" to="/">Login</Link></li>
-              <li className="nav-item"><Link className="nav-link text-white" to="/search">Book Search</Link></li>
-              <li className="nav-item"><Link className="nav-link text-white" to="/dashboard">Dashboard</Link></li>
-              <li className="nav-item"><Link className="nav-link text-white" to="/addbook">Add New Book</Link></li>
-            </ul>
-          </nav> */}
           </header>
           <main className="container mt-5">
             <Routes>
